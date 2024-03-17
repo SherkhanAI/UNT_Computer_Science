15aece561c1256757f01cccba4946a1ca6ca8ccf
SherkhanAI <142961336+SherkhanAI@users.noreply.github.com>
2024-03-17 21:26:08 +0500

    Create Project.sql

---
+++ SQL_Assigment1/Project.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Projects 
+(
+    Project_Name	VARCHAR(512),
+    Project_ID	INT,
+    Managing_Employee_ID	INT
+);
+
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project A', '101', '19');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project B', '102', '7');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project C', '103', '9');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project D', '104', '7');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project E', '105', '18');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project F', '106', '4');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project G', '107', '14');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project H', '108', '18');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project I', '109', '9');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project J', '110', '2');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project K', '111', '20');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project L', '112', '15');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project M', '113', '7');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project N', '114', '12');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project O', '115', '8');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project P', '116', '15');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project Q', '117', '3');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project R', '118', '14');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project S', '119', '17');
+INSERT INTO Projects (Project_Name, Project_ID, Managing_Employee_ID) VALUES ('Project T', '120', '4');
