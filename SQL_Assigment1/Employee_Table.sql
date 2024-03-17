4a3cebb8ccfa6a9d5d4625f9722c8a225621ba98
SherkhanAI <142961336+SherkhanAI@users.noreply.github.com>
2024-03-17 21:26:36 +0500

    Create Employee_Table.sql

---
+++ SQL_Assigment1/Employee_Table.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Employees 
+(
+    Employee_ID	INT,
+    Salary	INT,
+    Department	VARCHAR(512),
+    Name	VARCHAR(512)
+);
+
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('1', '65795', 'Sales', 'James Briggs');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('2', '50860', 'Marketing', 'Deanna Evans');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('3', '126820', 'IT', 'Deborah Salazar');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('4', '104886', 'Sales', 'Steven Tate');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('5', '56265', 'HR', 'Jacqueline Paul');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('6', '132386', 'Marketing', 'Austin Eaton');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('7', '87194', 'Sales', 'Daniel Vega');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('8', '137498', 'IT', 'Cynthia Diaz');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('9', '94131', 'IT', 'Martin Johnson');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('10', '110263', 'Finance', 'Jerry Smith');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('11', '66023', 'Finance', 'Tina Newman');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('12', '91090', 'HR', 'Ryan Thompson');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('13', '117221', 'Sales', 'Ryan Perkins');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('14', '114820', 'Sales', 'Mackenzie Mann');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('15', '50769', 'Finance', 'Stephanie Ruiz');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('16', '109735', 'Sales', 'Kayla Hoffman');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('17', '112955', 'Sales', 'Kenneth Jones');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('18', '114925', 'IT', 'Phillip Crawford');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('19', '117969', 'Finance', 'Kevin Bryan');
+INSERT INTO Employees (Employee_ID, Salary, Department, Name) VALUES ('20', '55311', 'Marketing', 'Sandra Shea');
